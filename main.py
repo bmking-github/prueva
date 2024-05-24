bd0d3d634c6b0354610d5f51c2269b44e24c11a4
Michael Martinez <163685041+bkmay1417@users.noreply.github.com>
2024-05-24 18:11:31 -0300

    sdas

--- main.py
+++ main.py
@@ -7,49 +7,33 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
 app = FastAPI(title='Proyecto Integrador I Hecho por Michael Martinez')
-
 # Cargar el dataset
 df_recom = pd.read_parquet(r'https://github.com/bkmay1417/Machine-Learning-Operations-MLOps-/blob/f079b6b85022872dbe96419294620b4f8d9aca14/Dataset/recomendacion3.parquet?raw=True')
-
-
 # Preprocesamiento de los géneros para generar la matriz TF-IDF
 vectorizer = TfidfVectorizer()
 tfidf_matrix = vectorizer.fit_transform(df_recom.groupby('item_id')['genres'].apply(lambda x: ' '.join(x)))
-
 # Cálculo de la similitud del coseno
 cosine_sim = cosine_similarity(tfidf_matrix, tfidf_matrix)
-
 @app.get("/Sistema_de_recomendacion")
 async def recomendacion_juego(item_id : float = Query(default=22330.0)):
-    """
-    Recomendación de juegos basada en el item_id.
-    El valor por defecto 10.0 corresponde a Counter-Strike.
-    """
     try:
         # Obtener el índice del juego en el DataFrame
         idx = df_recom[df_recom['item_id'] == item_id].index[0]
     except IndexError:
         # Manejar el caso donde no se encuentra el ID del juego
         raise HTTPException(status_code=404, detail="Item ID no encontrado.")
-
     # Calcular la similitud de coseno entre el juego dado y todos los juegos
     sim_scores = list(enumerate(cosine_sim[idx]))
-
     # Ordenar los juegos por su similitud de coseno
     sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)[1:6]
-
     # Obtener los índices de los juegos recomendados
     game_indices = [i[0] for i in sim_scores]
-
     # Obtener los títulos de los juegos recomendados
-    juegos_recomendados = df_recom['title'].iloc[game_indices].tolist()
-    
+    juegos_recomendados = df_recom['title'].iloc[game_indices].tolist() 
     return juegos_recomendados
 
-
-
 if name == "main":
-    port = int(os.environ.get('PORT', 8000)) 
-    uvicorn.run(app, host='0.0.0.0', port=port))
+    port = int(os.environ.get('PORT', 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
 
 
