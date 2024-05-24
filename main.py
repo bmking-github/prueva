c367928128b5aaf406aa1954b6b8888850465b6d
Michael Martinez <163685041+bkmay1417@users.noreply.github.com>
2024-05-24 18:17:52 -0300

    asdad

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ async def recomendacion_juego(item_id : float = Query(default=22330.0)):
     juegos_recomendados = df_recom['title'].iloc[game_indices].tolist() 
     return juegos_recomendados
 
-if name == "main":
+if __name__ == "__main__":
     port = int(os.environ.get('PORT', 8000))
     uvicorn.run(app, host="0.0.0.0", port=port)
 
